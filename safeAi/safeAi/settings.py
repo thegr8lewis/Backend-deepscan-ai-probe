1e2d464aa588d26f2d650a84686d2e6f01e60b6f
thegr8lewis <lewis.nyakaru@gmail.com>
2025-11-18 14:44:41 +0300

    updated the allowed host in settings file

--- safeAi/safeAi/settings.py
+++ safeAi/safeAi/settings.py
@@ -206,7 +206,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["ukweli-lens.onrender.com", "127.0.0.1", "localhost"]
 
 # ================================
 # APPLICATIONS
